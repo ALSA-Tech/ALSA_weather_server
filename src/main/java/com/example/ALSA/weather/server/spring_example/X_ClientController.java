20532afcdb4409cbcf56cdbe8ee721a40017648c
AntonGrn <45007883+AntonGrn@users.noreply.github.com>
2021-05-16 17:07:42 +0200

    Spring example, w dependency injection and JPA

---
+++ src/main/java/com/example/ALSA/weather/server/spring_example/X_ClientController.java
@@ -0,0 +1,45 @@
+package com.example.ALSA.weather.server.spring_example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/example/clients")
+public class X_ClientController {
+
+    private final X_ClientService service;
+
+    @Autowired // Autowire ClientService from the Spring container.
+    public X_ClientController(X_ClientService service) {
+        this.service = service;
+    }
+
+    @GetMapping()
+    public Iterable<X_Client> getClients() {
+        return service.getClients();
+    }
+
+    @GetMapping("/{id}")
+    public X_Client getClient(@PathVariable Integer id) throws X_ClientNotFoundException {
+        return service.getClient(id);
+    }
+
+    @PostMapping("/register")
+    public X_Client addClient(@RequestBody X_Client newClient) {
+        return service.addClient(newClient);
+    }
+
+    @PostMapping("/update")
+    public X_Client updateClient(@RequestBody X_Client updatedClient) {
+        return service.updateClient(updatedClient);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteClient(@PathVariable Integer id) {
+        service.deleteClient(id);
+    }
+
+
+}
