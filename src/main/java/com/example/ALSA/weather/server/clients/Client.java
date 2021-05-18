58da1b3f3ded4b45412a3006242df8ebe3c6e4c7
Ali Muhammed <Alawi93@live.se>
2021-05-18 15:34:57 +0200

    clientModel variables

--- src/main/java/com/example/ALSA/weather/server/clients/Client.java
+++ src/main/java/com/example/ALSA/weather/server/clients/Client.java
@@ -1,29 +1,27 @@
 package com.example.ALSA.weather.server.clients;
 
 import javax.persistence.*;
-
+import java.util.ArrayList;
 @Entity // For Hibernate. Create JPA Entity.
 @Table(name = "alsa_clients") // ORM: Mapping object to DB table.
 public class Client {
-//Todo client variables
-//TODO:  No args constructor. Needed for DB ORM
-//TODO:  All args constructor
-//TODO:  GETTERS
-
     @Id // Primary key
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Needed to auto increment id in DB.
-
-    private int id; // string id  instead?
+    private int id;
     private String name;
     private String password;
+    private String email;
+    
+    //private ArrayList<String> locationSubscriptions;
 
     public Client (){
     }
 
-    public Client(int id, String name, String password) {
+    public Client(int id, String name, String password, String email) {
         this.id = id;
         this.name = name;
         this.password = password;
+        this.email = email;
     }
 
     public int getId() {
@@ -37,4 +35,13 @@ public class Client {
     public String getPassword() {
         return password;
     }
+
+    public String getEmail() {
+        return email;
+    }
 }
+
+//Todo client variables
+//TODO:  No args constructor. Needed for DB ORM
+//TODO:  All args constructor
+//TODO:  GETTERS
